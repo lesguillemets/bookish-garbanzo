99afe8660470af38cca42e9555bf6b5656febb60
lesguillemets <lesguillemets@users.noreply.github.com>
2016-06-19 17:56:10 +0900

    can't seem to exit

--- talk/Main.hs
+++ talk/Main.hs
@@ -4,67 +4,23 @@ module Main where
 import Lib
 import Data.Conduit
 import Data.Conduit.Network
-import Data.Conduit.Text
-import qualified Data.ByteString.Char8 as BC
-import Control.Monad.IO.Class
-import Control.Lens
-import Data.Aeson.Lens
-import qualified Data.Text as T
-import Data.Text (Text)
-import qualified Data.Text.IO as TI
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
-import qualified Data.Streaming.Network as SN
-import qualified GHC.Conc as Conc
+import Data.ByteString (ByteString)
+import Control.Monad.IO.Class (liftIO)
 import System.Exit (exitSuccess)
 
-jsonString :: BC.ByteString
-jsonString = "[ 1, \"Hello world\"]"
-
 main :: IO ()
-main = do
-    runTCPServer (serverSettings defaultPort "*") runnr
+main = runTCPServer (serverSettings defaultPort "*") $ \ appData ->
+        appSource appData $$ conduit =$= appSink appData
 
-runnr appData =
-    appSource appData $$ decode utf8
-                      =$= conduit
-                      =$= encode utf8
-                      =$= appSink' appData
-
-appSink' ad = do
-    c <- await
-    case c of
-         Nothing -> liftIO $ do
-             putStrLn "Quit!"
-             exitSuccess
-         Just x -> do
-             liftIO $ SN.appWrite ad x >> Conc.yield
-             appSink' ad
-
-logYield s = do
-    liftIO $ do
-        TI.putStr "Probably sending\t"
-        TI.putStrLn s
-    yield s
-
-conduit :: ConduitM Text Text IO ()
+conduit :: ConduitM ByteString ByteString IO ()
 conduit = do
     str <- await
     case str of
          Nothing -> liftIO $ do
              putStrLn "Nothing left"
-         (Just s) -> do
-             logYield . processmsg $ s
-             conduit
-
-processmsg :: Text -> Text
-processmsg msg =
-    T.concat [
-        "[",
-        T.pack . show $ n,
-        ",\"",
-        T.reverse txt,
-        "\"]"
-    ]
-    where
-        Just n = msg ^? nth 0 . _Integral
-        Just txt = msg ^? nth 1 . _String
+             exitSuccess
+         (Just s) -> case s of
+                          "quit\n" -> liftIO $ putStrLn "Quit?" *> exitSuccess
+                          _ -> do
+                              yield s
+                              conduit
